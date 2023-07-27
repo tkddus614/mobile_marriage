e1dc4e8a864c430fe13dde1ab7a61abf541c1f24
Sangyeon Kim <128672067+tkddus@users.noreply.github.com>
2023-07-27 22:40:25 +0900

    chore: 중간저장

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,109 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import Image from "next/image"
+import { styled } from "styled-components"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Container>
+      <Header>
+        <div>김상연,<br />최은빈</div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Image src='/calendar.svg' width={47} height={52} alt='date' />
         </div>
-      </div>
+      </Header>
+      <hr style={{color: '#C2C2C2', marginTop: '22px', width: '49px'}}/>
+      <HeaderDesc>
+      2023년 9월 16일 토요일 오후 1시 30분<br />네스트웨딩홀1층 시에나홀
+      </HeaderDesc>
+      <MainImage />
+      <MarryContainer>
+        <MarryInfo>
+        <div className="line">결혼합니다</div>
+        <div className="desc">두 사람이 하나가 된 새 인생을 시작합니다.<br />사랑으로 가득 채워<br />즐거움은 나누고 어려움은 이겨내는 함께<br />나아가는 삶을 꾸리겠습니다.<br />부디 걸음하시어 축복하여 주시면 더 없는<br />기쁨이 되겠습니다.</div>
+        <hr style={{color: '#D1C5AD', width: '80%', margin: '25px auto'}}/>
+        <div className="baby">김도영 · 임현미의&nbsp;&nbsp;&nbsp;장남&nbsp;&nbsp;&nbsp;&nbsp;상연</div>
+        <div className="baby">최종덕 · 김혜정의&nbsp;&nbsp;&nbsp;장녀&nbsp;&nbsp;&nbsp;&nbsp;은빈</div>
+        </MarryInfo>
+      </MarryContainer>
+      <CalendarWrap>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </CalendarWrap>
+    </Container>
   )
 }
+
+const CalendarWrap = styled.div`
+  background: #F6F5F5;
+  padding: 47px 20px;
+
+`
+
+const MarryInfo = styled.div`
+  border: 1px solid #D1C5AD;
+  position: relative;
+  padding-bottom: 37px;
+  > div {
+    &.line {
+    position: absolute;
+    width: 150px;
+    top: -20px;
+    left: 50%;
+    z-index: 2;
+    background: #EFE2D5;
+    transform: translateX(-50%); /* 가운데 정렬을 위해 이동 */
+    font-size: 25px;
+    margin: 8px 0px;
+    }
+    &.desc {
+    padding-top: 45px;
+    font-family: 'Cafe24';
+    font-size: 18px;
+    line-height: 28px; /* 155.556% */
+  }
+  &.baby {
+    font-family: 'Cafe24';
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 28px;
+  }
+}
+`
+
+const MarryContainer = styled.div`
+  margin-top: 20px;
+  background: #EFE2D5;
+  text-align: center;
+  padding: 55px 20px;
+  text-align: center;
+  margin: 20px -1rem;
+`;
+
+const MainImage = styled.div`
+  width: 100%;
+  height: 500px;
+  margin-top: 15px;
+  background: url('/main.png') no-repeat;
+  background-size: contain;
+`
+
+const HeaderDesc = styled.div`
+  margin-top: 18px;
+  color: #A68658;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px; 
+`
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  color: #000;
+  font-size: 25px;
+  font-weight: 400;
+  line-height: 34px; 
+`
+
+const Container = styled.main`
+  padding: 2rem 1rem;
+`
