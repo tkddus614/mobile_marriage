cf947b8cd646a27d9e1289992d9f9bf29bf23941
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2023-08-11 11:51:54 +0900

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from 'next/head'
 
 export const metadata = {
   title: 'Mobile Wedding',
@@ -8,6 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <Head>
+        <meta property="og:url" content="https://mobile-marriage.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="모바일 청첩장" />
+        <meta property="og:description" content="김상연 최은빈 결혼합니다." />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/73007012/259082689-ba6eb18d-b20b-426c-82c9-69b11de79a60.png" />
+      </Head>
       <body>{children}</body>
     </html>
   )
