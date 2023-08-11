a2680a02758314114ed3a826f1da235cb230cc25
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2023-08-11 11:46:01 +0900

    Update _document.js

--- src/_document.js
+++ src/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta property="og:url" content="https://mobile-marriage.vercel.app" />
+          <meta property="og:type" content="website" />
           <meta property="og:title" content="모바일 청첩장" />
           <meta property="og:description" content="김상연 최은빈 결혼합니다." />
           <meta property="og:image" content="https://user-images.githubusercontent.com/73007012/259082689-ba6eb18d-b20b-426c-82c9-69b11de79a60.png" />
